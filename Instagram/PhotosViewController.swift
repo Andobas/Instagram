260ff8f690d8c59292e8c78037626ff7992ec924
Andobas <juanhernandez@Juans-MacBook-Pro.local>
2016-01-21 20:30:56 -0800

    Milestone 2
    Codepath Week 1 Lab

---
+++ Instagram/PhotosViewController.swift
@@ -0,0 +1,60 @@
+//
+//  PhotosViewController.swift
+//  Instagram
+//
+//  Created by Juan Hernandez on 1/21/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    var photos: [NSDictionary]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            self.photos = responseDictionary["data"] as? [NSDictionary]
+                    }
+                }
+        });
+        task.resume()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
